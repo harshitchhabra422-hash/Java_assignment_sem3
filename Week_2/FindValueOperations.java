f1da23c5903ceca801ab4144a89e26f31f582eef
harshitchhabra422-hash <harshitchhabra422@gmail.com>
2026-08-16 18:05:01 +0530

    FindValueOperations.java

--- Week_2/FindValueOperations.java
+++ Week_2/FindValueOperations.java
@@ -1,14 +1,1 @@
-public class FinalValueOperations {
-    public int finalValueAfterOperations(String[] operations) {
-        int X = 0;
-        for(String op : operations){
-            if(op.equals("++X") || op.equals("X++")){
-                X = X + 1;
-            }
-            else if(op.equals("--X") || op.equals("X--")){
-                X = X - 1;
-            }
-        }
-        return X;
-    }
-}
+
