acb8eb8ffe88dfcb0da7f56b1e6d427012db4273
harshitchhabra422-hash <harshitchhabra422@gmail.com>
2026-08-16 18:21:11 +0530

    MarksAnalyzer.java

---
+++ Week_2/MarksAnalyzer.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class NumberAnalyzer {
+
+    static int readNumber(Scanner input) {
+        System.out.print("Enter a number: ");
+        return input.nextInt();
+    }
+
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    static boolean isPositive(int number) {
+        return number > 0;
+    }
+
+    static int absoluteValue(int number) {
+        return Math.abs(number);
+    }
+
+    static int countDigits(int number) {
+
+        if (number == 0) {
+            return 1;
+        }
+
+        int count = 0;
+
+        while (number != 0) {
+            number = number / 10;
+            count++;
+        }
+
+        return count;
+    }
+
+    static int sumDigits(int number) {
+
+        int sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+
+        return sum;
+    }
+
+    static void printReport(
+            int number,
+            boolean even,
+            boolean positive,
+            int digitCount,
+            int digitSum) {
+
+        System.out.println("Number = " + number);
+        System.out.println("Even = " + even);
+        System.out.println("Positive = " + positive);
+        System.out.println("Number of digits = " + digitCount);
+        System.out.println("Sum of digits = " + digitSum);
+    }
+}
